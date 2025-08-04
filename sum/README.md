8c3e29a1c55c285bd788ef55fbb79c3ad84883c4
Tobias Hort <mail@tobiashort.com>
2025-08-04 20:01:56 +0200

    sum

---
+++ sum/README.md
@@ -0,0 +1,11 @@
+```
+Reads from Stdin and counts together all numeric values
+
+Usage:
+  sum [OPTIONS]
+
+Options:
+  -i, --ignore-nan  Ignores not numeric values and treats them as 0.
+  -h, --help        Show this help message and exit
+
+```
---
+++ sum/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/big"
+	"os"
+
+	"github.com/tobiashort/clap-go"
+)
+
+type Args struct {
+	IgnoreNaN bool `clap:"long=ignore-nan,description='Ignores not numeric values and treats them as 0.'"`
+}
+
+func main() {
+	args := Args{}
+	clap.Description("Reads from Stdin and counts together all numeric values")
+	clap.Parse(&args)
+
+	sum := big.NewFloat(0)
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		text := scanner.Text()
+		num, ok := new(big.Float).SetString(text)
+		if ok {
+			sum = sum.Add(sum, num)
+		} else {
+			if !args.IgnoreNaN {
+				fmt.Fprintf(os.Stderr, "%s is not a number", text)
+				os.Exit(1)
+			}
+		}
+	}
+
+	fmt.Println(sum)
+}
