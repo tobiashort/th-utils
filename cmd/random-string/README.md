7f66c00cb9d8191f05d043d64954e3ac39650ece
Tobias Hort <mail@tobiashort.com>
2025-08-06 20:51:04 +0200

    random-string

--- cmd/random-string/README.md
+++ cmd/random-string/README.md
@@ -1,21 +1,25 @@
 ```
-Usage: rand-string [-w WIDTH] [-l] [-u] [-n] [-s] [-a ALPHABET] [-c COUNT]
-Generates random strings.
+Usage:
+  random-string [OPTIONS]
 
+Options:
+  -w, --width <Width>        Width in characters (default: 12)
+  -l, --lowercase            Use lowercase characters
+  -u, --uppercase            Use uppercase characters
+  -n, --numbers              Use numbers
+  -s, --symbols              Use symbols
+  -x, --hex                  Use hexadecimals as the alphabet
+  -a, --alphabet <Alphabet>  The custom alphabet to be used
+  -c, --amount <Amount>      The amount of strings to be generated (default: 1)
+  -h, --help                 Show this help message and exit
 
-EXAMPLES
-
-	./rand-string -l -u -n -w 12 -c 100
-	Generates 100 twelve character wide random strings with
-	lowercase and uppercase characters and numbers.
-
-
-	./rand-string -a "abc123" -w 10 -c 50
-	Generates 50 ten character wide random strings that match
-	the given alphabet.
-
-
-FLAGS
-
+Example:
+  $ rand-string -lun -w 12 -c 100
+  Generates 100 twelve character wide random strings with
+  lowercase and uppercase characters and numbers.
+  
+  $ rand-string -a "abc123" -w 10 -c 50
+  Generates 50 ten character wide random strings that match
+  the given alphabet.
 
 ```
--- cmd/random-string/main.go
+++ cmd/random-string/main.go
@@ -1,35 +1,13 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"math/rand"
 	"os"
 	"time"
+
+	"github.com/tobiashort/clap-go"
 )
-
-func usage() {
-	fmt.Print(`Usage: rand-string [-w WIDTH] [-l] [-u] [-n] [-s] [-a ALPHABET] [-c COUNT]
-Generates random strings.
-
-
-EXAMPLES
-
-	./rand-string -l -u -n -w 12 -c 100
-	Generates 100 twelve character wide random strings with
-	lowercase and uppercase characters and numbers.
-
-
-	./rand-string -a "abc123" -w 10 -c 50
-	Generates 50 ten character wide random strings that match
-	the given alphabet.
-
-
-FLAGS
-
-`)
-	flag.PrintDefaults()
-}
 
 const (
 	Symbols      = "!\"#$%&'()*+,-./:;<>=?@[\\]^_`{|}~"
@@ -39,56 +17,57 @@ const (
 	Hexadecimals = Numbers + "abcdef"
 )
 
+type Args struct {
+	Width     int    `clap:"default-value=12,description='Width in characters'"`
+	Lowercase bool   `clap:"description='Use lowercase characters'"`
+	Uppercase bool   `clap:"description='Use uppercase characters'"`
+	Numbers   bool   `clap:"description='Use numbers'"`
+	Symbols   bool   `clap:"description='Use symbols'"`
+	Hex       bool   `clap:"short=x,conflicts-with='Alphabet,Lowercase,Uppercase,Numbers,Symbols',description='Use hexadecimals as the alphabet'"`
+	Alphabet  string `clap:"conflicts-with='Hex,Lowercase,Uppercase,Numbers,Symbols',description='The custom alphabet to be used'"`
+	Amount    int    `clap:"short=c,default-value=1,description='The amount of strings to be generated'"`
+}
+
 func main() {
-	var flagWidth int
-	var flagLowercase bool
-	var flagUppercase bool
-	var flagNumbers bool
-	var flagSymbols bool
-	var flagHex bool
-	var flagAlphabet string
-	var flagCount int
+	args := Args{}
 
-	flag.Usage = usage
-	flag.IntVar(&flagWidth, "w", 12, "width in characters")
-	flag.BoolVar(&flagLowercase, "l", false, "lowercase characters [a-z]")
-	flag.BoolVar(&flagUppercase, "u", false, "uppercase characters [A-Z]")
-	flag.BoolVar(&flagNumbers, "n", false, "numbers [0-9]")
-	flag.BoolVar(&flagSymbols, "s", false, fmt.Sprintf("symbols [%s]", Symbols))
-	flag.BoolVar(&flagHex, "H", false, "hexadecimals [0-9a-f]")
-	flag.StringVar(&flagAlphabet, "a", "", "alphabet to be used instead")
-	flag.IntVar(&flagCount, "c", 1, "the amount of strings to be generated")
-	flag.Parse()
+	clap.Example(`$ rand-string -lun -w 12 -c 100
+Generates 100 twelve character wide random strings with
+lowercase and uppercase characters and numbers.
 
-	if flagWidth < 1 {
+$ rand-string -a "abc123" -w 10 -c 50
+Generates 50 ten character wide random strings that match
+the given alphabet.`)
+
+	clap.Parse(&args)
+
+	if args.Width < 1 {
 		fmt.Fprintf(os.Stderr, "w must be greater than 0")
-		usage()
 		os.Exit(1)
 	}
 
-	if flagCount < 1 {
+	if args.Amount < 1 {
 		fmt.Fprintf(os.Stderr, "c must be greater than 0")
-		usage()
 		os.Exit(1)
 	}
 
 	var alphabet string
 
-	if flagAlphabet != "" {
-		alphabet = flagAlphabet
-	} else if flagHex {
+	if args.Alphabet != "" {
+		alphabet = args.Alphabet
+	} else if args.Hex {
 		alphabet = Hexadecimals
 	} else {
-		if flagLowercase {
+		if args.Lowercase {
 			alphabet += Lowercase
 		}
-		if flagUppercase {
+		if args.Uppercase {
 			alphabet += Uppercase
 		}
-		if flagNumbers {
+		if args.Numbers {
 			alphabet += Numbers
 		}
-		if flagSymbols {
+		if args.Symbols {
 			alphabet += Symbols
 		}
 	}
@@ -99,9 +78,9 @@ func main() {
 
 	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
 
-	for range flagCount {
-		b := make([]byte, flagWidth)
-		for i := range flagWidth {
+	for range args.Amount {
+		b := make([]byte, args.Width)
+		for i := range args.Width {
 			b[i] = alphabet[rnd.Intn(len(alphabet))]
 		}
 		fmt.Println(string(b))
