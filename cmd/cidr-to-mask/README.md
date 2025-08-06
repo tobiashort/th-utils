bff5fafcd260708c1cb507a0ea6aea4abf09ecd7
Tobias Hort <mail@tobiashort.com>
2025-08-06 08:34:41 +0200

    cidr-to-mask

---
+++ cmd/cidr-to-mask/README.md
@@ -0,0 +1,11 @@
+```
+Usage:
+  cidr-to-mask [OPTIONS] <CIDR>
+
+Options:
+  -h, --help  Show this help message and exit
+
+Positional arguments:
+  CIDR        The CIDR to be converted to subnet mask (required)
+
+```
--- cmd/cidr-to-mask/main.go
+++ cmd/cidr-to-mask/main.go
@@ -1,61 +1,34 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"io"
 	"os"
-	"strconv"
-	"strings"
+
+	"github.com/tobiashort/clap-go"
 )
 
-func printUsage() {
-	fmt.Fprint(os.Stderr, `Usage: cidr-to-mask [CIDR]
-Reads from STDIN if CIDR is not provided as an argument.
-
-Flags:
-`)
-	flag.PrintDefaults()
-	os.Exit(-1)
+type Args struct {
+	CIDR int `clap:"positional,mandatory,description='The CIDR to be converted to subnet mask'"`
 }
 
 func printInvalid(input string) {
-	fmt.Fprintf(os.Stderr, "Invalid input '%s'\n", input)
-	os.Exit(-1)
 }
 
 func main() {
-	flag.Usage = printUsage
-	help := flag.Bool("h", false, "print help")
-	flag.Parse()
-	if *help {
-		printUsage()
-		return
+	args := Args{}
+	clap.Parse(&args)
+
+	if args.CIDR < 0 || args.CIDR > 32 {
+		fmt.Fprintf(os.Stderr, "Invalid input '%d'. Must be inbetween 0 and 32.\n", args.CIDR)
+		os.Exit(1)
 	}
-	if len(os.Args) > 2 {
-		printUsage()
-		return
-	}
-	input := ""
-	if len(os.Args) == 2 {
-		input = os.Args[1]
-	} else {
-		data, err := io.ReadAll(os.Stdin)
-		if err != nil {
-			panic(err)
-		}
-		input = strings.TrimSpace(string(data))
-	}
-	cidr, err := strconv.Atoi(input)
-	if err != nil || cidr < 0 || cidr > 32 {
-		printInvalid(input)
-		return
-	}
+
 	mask := uint32(0b11111111_11111111_11111111_11111111)
-	mask = (mask << (32 - cidr)) & mask
+	mask = (mask << (32 - args.CIDR)) & mask
 	octet0 := uint8(mask >> 24)
 	octet1 := uint8(mask >> 16)
 	octet2 := uint8(mask >> 8)
 	octet3 := uint8(mask)
+
 	fmt.Printf("%d.%d.%d.%d\n", octet0, octet1, octet2, octet3)
 }
