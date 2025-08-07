b2976112584fa90273555de3360e7429f4d6bd83
Tobias Hort <mail@tobiashort.com>
2025-08-07 18:17:52 +0200

    rfc3339-to-unixtime

--- cmd/rfc3339-to-unixtime/main.go
+++ cmd/rfc3339-to-unixtime/main.go
@@ -1,57 +1,43 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"io"
 	"os"
-	"strings"
 	"time"
+
+	"github.com/tobiashort/clap-go"
 )
 
-func printUsage() {
-	fmt.Fprint(os.Stderr, `Usage: rfc33392unixtime [RFC3339]
-Reads from STDIN if RFC3339 is not provided as an argument.
-
-Flags:
-`)
-	flag.PrintDefaults()
+func invalidInput(input string) {
+	fmt.Fprintf(os.Stderr, "the provided input is not in RFC3339 format, e.g. 1970-01-01T00:00:00Z: %s", input)
 	os.Exit(1)
 }
 
-func invalidInput(input string) {
-	fmt.Fprintf(os.Stderr, `The provided input is not in RFC3339 format: %s
-Example: 1970-01-01T00:00:00Z
-`, input)
-	os.Exit(1)
+type Args struct {
+	RFC3339 string `clap:"positional,description='The datetime in RFC3339 format, e.g. 1970-01-01T00:00:00Z. Reads from Stdin if not specified.'"`
 }
 
 func main() {
-	help := flag.Bool("h", false, "help")
-	flag.Parse()
-	if *help {
-		printUsage()
-		return
-	}
-	if flag.NArg() > 1 {
-		printUsage()
-		return
-	}
-	input := ""
-	if flag.NArg() == 1 {
-		input = flag.Arg(0)
-	} else if flag.NArg() == 0 {
-		data, err := io.ReadAll(os.Stdin)
+	args := Args{}
+	clap.Parse(&args)
+
+	var input string
+	if args.RFC3339 != "" {
+		input = args.RFC3339
+	} else {
+		read, err := io.ReadAll(os.Stdin)
 		if err != nil {
 			panic(err)
 		}
-		input = string(data)
-		input = strings.TrimSpace(input)
+		input = string(read)
 	}
+
 	rfc3339, err := time.Parse(time.RFC3339, input)
 	if err != nil {
 		invalidInput(input)
 		return
 	}
+
 	fmt.Println(rfc3339.Unix())
 }
