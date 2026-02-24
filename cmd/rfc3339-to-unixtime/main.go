8503c036a0b987c0d02d7f90b9e3aa9fa09426d2
Tobias Hort <tobias.hort-giess@compass-security.com>
2026-02-24 12:55:53 +0100

    read from stdin

--- cmd/rfc3339-to-unixtime/main.go
+++ cmd/rfc3339-to-unixtime/main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"time"
 
 	"github.com/tobiashort/clap-go"
+	"github.com/tobiashort/utils-go/must"
 )
 
 type Args struct {
-	RFC3339 string `clap:"positional,mandatory,desc='The datetime in RFC3339 format, e.g. 1970-01-01T00:00:00Z'"`
+	RFC3339 string `clap:"positional,desc='The datetime in RFC3339 format, e.g. 1970-01-01T00:00:00Z. Reads from Stdin if not specified.'"`
 }
 
 func main() {
@@ -17,6 +19,10 @@ func main() {
 	clap.Parse(&args)
 
 	input := args.RFC3339
+	if input == "" {
+		input = string(must.Do2(io.ReadAll(os.Stdin)))
+	}
+
 	rfc3339, err := time.Parse(time.RFC3339, input)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "the provided input is not in RFC3339 format, e.g. 1970-01-01T00:00:00Z: %s", input)
--- cmd/unixtime-to-rfc3339/main.go
+++ cmd/unixtime-to-rfc3339/main.go
@@ -2,17 +2,28 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/tobiashort/clap-go"
+	"github.com/tobiashort/utils-go/must"
 )
 
 type Args struct {
-	Secs int `clap:"positional,mandatory,desc='The seconds to convert to RFC3339 format'"`
+	Secs string `clap:"positional,desc='The seconds to convert to RFC3339 format. Reads from Stdin if not specified.'"`
 }
 
 func main() {
 	args := Args{}
 	clap.Parse(&args)
-	fmt.Println(time.Unix(int64(args.Secs), 0).Format(time.RFC3339))
+
+	input := args.Secs
+	if input == "" {
+		input = string(must.Do2(io.ReadAll(os.Stdin)))
+	}
+
+	secs := must.Do2(strconv.Atoi(input))
+	fmt.Println(time.Unix(int64(secs), 0).Format(time.RFC3339))
 }
