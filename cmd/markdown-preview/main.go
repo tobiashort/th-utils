7bb3d0193bfb4dd50d744392f8fd0e30a56d9435
Tobias Hort <tobias.hort-giess@compass-security.com>
2026-03-13 14:10:55 +0100

    add markdown-preview

---
+++ cmd/markdown-preview/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"runtime"
+
+	"github.com/tobiashort/clap-go"
+	"github.com/tobiashort/clog-go"
+)
+
+type Args struct {
+	File string `clap:"positional,mandatory,desc='The file to preview'"`
+}
+
+func run() int {
+	args := Args{}
+	clap.Parse(&args)
+
+	pth, err := exec.LookPath("pandoc")
+	if err != nil {
+		clog.Error("pandoc: #r{not in path}")
+		return 1
+	}
+	clog.Infof("pandoc: #g{%s}", pth)
+
+	tmp, err := os.CreateTemp("", "markdown-preview_*.html")
+	if err != nil {
+		clog.Error("create temp:", err)
+		return 1
+	}
+	defer os.Remove(tmp.Name())
+
+	var cmd *exec.Cmd
+
+	cmd = exec.Command("pandoc", "-o", tmp.Name(), args.File)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		return 1
+	}
+
+	switch runtime.GOOS {
+	case "linux":
+		cmd = exec.Command("xdg-open", tmp.Name())
+	case "windows":
+		cmd = exec.Command("start", tmp.Name())
+	case "darwin":
+		cmd = exec.Command("open", tmp.Name())
+	default:
+		clog.Error("unknown GOOS:", runtime.GOOS)
+		return 1
+	}
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		return 1
+	}
+
+	return 0
+}
+
+func main() {
+	os.Exit(run())
+}
