0f07ff82cde95e327c6a7aab7e17062c25291960
Tobias Hort <mail@tobiashort.com>
2026-03-29 22:10:09 +0200

    ForceColor
    Allow to enforce colors even if IsTerminal is false

--- lib/cfmt/cfmt.go
+++ lib/cfmt/cfmt.go
@@ -12,63 +12,75 @@ import (
 	"github.com/tobiashort/th-utils/lib/term"
 )
 
+type Formatter struct {
+	ForceColors bool
+}
+
+var DefaultFormatter = Formatter{
+	ForceColors: false,
+}
+
 var re = regexp.MustCompile(`#([A-Za-z]{1,2})\{((?:\\.|[^\\}])*)\}`)
 
-func Print(a ...any) {
+func (f Formatter) inColor() bool {
+	return term.IsTerminal() || f.ForceColors
+}
+
+func (f Formatter) Print(a ...any) {
 	for i := range a {
-		a[i] = clr(fmt.Sprint(a[i]), ansi.DecorReset)
+		a[i] = f.clr(fmt.Sprint(a[i]), ansi.DecorReset)
 	}
 	fmt.Print(a...)
 }
 
-func Printf(format string, a ...any) {
-	fmt.Printf(clr(format, ansi.DecorReset), a...)
+func (f Formatter) Printf(format string, a ...any) {
+	fmt.Printf(f.clr(format, ansi.DecorReset), a...)
 }
 
-func Println(a ...any) {
+func (f Formatter) Println(a ...any) {
 	for i := range a {
-		a[i] = clr(fmt.Sprint(a[i]), ansi.DecorReset)
+		a[i] = f.clr(fmt.Sprint(a[i]), ansi.DecorReset)
 	}
 	fmt.Println(a...)
 }
 
-func Fprint(w io.Writer, a ...any) {
+func (f Formatter) Fprint(w io.Writer, a ...any) {
 	for i := range a {
-		a[i] = clr(fmt.Sprint(a[i]), ansi.DecorReset)
+		a[i] = f.clr(fmt.Sprint(a[i]), ansi.DecorReset)
 	}
 	fmt.Fprint(w, a...)
 }
 
-func Fprintf(w io.Writer, format string, a ...any) {
-	fmt.Fprintf(w, clr(format, ansi.DecorReset), a...)
+func (f Formatter) Fprintf(w io.Writer, format string, a ...any) {
+	fmt.Fprintf(w, f.clr(format, ansi.DecorReset), a...)
 }
 
-func Fprintln(w io.Writer, a ...any) {
+func (f Formatter) Fprintln(w io.Writer, a ...any) {
 	for i := range a {
-		a[i] = clr(fmt.Sprint(a[i]), ansi.DecorReset)
+		a[i] = f.clr(fmt.Sprint(a[i]), ansi.DecorReset)
 	}
 	fmt.Fprintln(w, a...)
 }
 
-func Sprint(a ...any) string {
+func (f Formatter) Sprint(a ...any) string {
 	for i := range a {
-		a[i] = clr(fmt.Sprint(a[i]), ansi.DecorReset)
+		a[i] = f.clr(fmt.Sprint(a[i]), ansi.DecorReset)
 	}
 	return fmt.Sprint(a...)
 }
 
-func Sprintf(format string, a ...any) string {
-	return fmt.Sprintf(clr(format, ansi.DecorReset), a...)
+func (f Formatter) Sprintf(format string, a ...any) string {
+	return fmt.Sprintf(f.clr(format, ansi.DecorReset), a...)
 }
 
-func Sprintln(a ...any) string {
+func (f Formatter) Sprintln(a ...any) string {
 	for i := range a {
-		a[i] = clr(fmt.Sprint(a[i]), ansi.DecorReset)
+		a[i] = f.clr(fmt.Sprint(a[i]), ansi.DecorReset)
 	}
 	return fmt.Sprintln(a...)
 }
 
-func stoc(s string) (ansi.Decor, error) {
+func (f Formatter) stoc(s string) (ansi.Decor, error) {
 	//nofmt:enable
 	switch s {
 	case "r":  return ansi.DecorRed, nil
@@ -103,66 +115,66 @@ func stoc(s string) (ansi.Decor, error) {
 	//nofmt:disable
 }
 
-func Cprint(color string, a ...any) {
+func (f Formatter) Cprint(color string, a ...any) {
 	Cfprint(os.Stdout, color, a...)
 }
 
-func Cfprint(w io.Writer, color string, a ...any) {
-	c := must.Do2(stoc(color))
+func (f Formatter) Cfprint(w io.Writer, color string, a ...any) {
+	c := must.Do2(f.stoc(color))
 	for i := range a {
-		a[i] = clr(fmt.Sprint(a[i]), c)
+		a[i] = f.clr(fmt.Sprint(a[i]), c)
 	}
-	if term.IsTerminal() {
+	if f.inColor() {
 		fmt.Fprint(w, c)
 	}
 	fmt.Fprint(w, a...)
-	if term.IsTerminal() {
+	if f.inColor() {
 		fmt.Fprint(w, ansi.DecorReset)
 	}
 }
 
-func Cprintf(color string, format string, a ...any) {
+func (f Formatter) Cprintf(color string, format string, a ...any) {
 	Cfprintf(os.Stdout, color, format)
 }
 
-func Cfprintf(w io.Writer, color string, format string, a ...any) {
-	c := must.Do2(stoc(color))
-	if term.IsTerminal() {
+func (f Formatter) Cfprintf(w io.Writer, color string, format string, a ...any) {
+	c := must.Do2(f.stoc(color))
+	if f.inColor() {
 		fmt.Fprint(w, c)
 	}
-	fmt.Fprintf(w, clr(format, c), a...)
-	if term.IsTerminal() {
+	fmt.Fprintf(w, f.clr(format, c), a...)
+	if f.inColor() {
 		fmt.Fprint(w, ansi.DecorReset)
 	}
 }
 
-func Cprintln(color string, a ...any) {
+func (f Formatter) Cprintln(color string, a ...any) {
 	Cfprintln(os.Stdout, color, a...)
 }
 
-func Cfprintln(w io.Writer, color string, a ...any) {
-	c := must.Do2(stoc(color))
+func (f Formatter) Cfprintln(w io.Writer, color string, a ...any) {
+	c := must.Do2(f.stoc(color))
 	for i := range a {
-		a[i] = clr(fmt.Sprint(a[i]), c)
+		a[i] = f.clr(fmt.Sprint(a[i]), c)
 	}
-	if term.IsTerminal() {
+	if f.inColor() {
 		fmt.Fprint(w, c)
 	}
 	fmt.Fprintln(w, a...)
-	if term.IsTerminal() {
+	if f.inColor() {
 		fmt.Fprint(w, ansi.DecorReset)
 	}
 }
 
-func clr(str string, reset ansi.Decor) string {
+func (f Formatter) clr(str string, reset ansi.Decor) string {
 	matches := re.FindAllStringSubmatch(str, -1)
 	for _, match := range matches {
-		if c, err := stoc(match[1]); err == nil {
+		if c, err := f.stoc(match[1]); err == nil {
 			replace := match[0]
 			replaceWith := match[2]
 			replaceWith = strings.ReplaceAll(replaceWith, `\}`, `}`)
 			replaceWith = strings.ReplaceAll(replaceWith, `\\`, `\`)
-			if term.IsTerminal() {
+			if f.inColor() {
 				str = strings.Replace(str, replace, c+replaceWith+reset, 1)
 			} else {
 				str = strings.Replace(str, replace, replaceWith, 1)
@@ -172,14 +184,82 @@ func clr(str string, reset ansi.Decor) string {
 	return str
 }
 
-func Begin(decor ansi.Decor) {
-	if term.IsTerminal() {
+func (f Formatter) Begin(decor ansi.Decor) {
+	if f.inColor() {
 		fmt.Print(decor)
 	}
 }
 
-func End() {
-	if term.IsTerminal() {
+func (f Formatter) End() {
+	if f.inColor() {
 		fmt.Print(ansi.DecorReset)
 	}
+}
+
+func Print(a ...any) {
+	DefaultFormatter.Print(a...)
+}
+
+func Printf(format string, a ...any) {
+	DefaultFormatter.Printf(format, a...)
+}
+
+func Println(a ...any) {
+	DefaultFormatter.Println(a...)
+}
+
+func Fprint(w io.Writer, a ...any) {
+	DefaultFormatter.Fprint(w, a...)
+}
+
+func Fprintf(w io.Writer, format string, a ...any) {
+	DefaultFormatter.Fprintf(w, format, a...)
+}
+
+func Fprintln(w io.Writer, a ...any) {
+	DefaultFormatter.Fprintln(w, a...)
+}
+
+func Sprint(a ...any) string {
+	return DefaultFormatter.Sprint(a...)
+}
+
+func Sprintf(format string, a ...any) string {
+	return DefaultFormatter.Sprintf(format, a...)
+}
+
+func Sprintln(a ...any) string {
+	return DefaultFormatter.Sprintln(a...)
+}
+
+func Cprint(color string, a ...any) {
+	DefaultFormatter.Cprint(color, a...)
+}
+
+func Cfprint(w io.Writer, color string, a ...any) {
+	DefaultFormatter.Cfprint(w, color, a...)
+}
+
+func Cprintf(color string, format string, a ...any) {
+	DefaultFormatter.Cprintf(color, format, a...)
+}
+
+func Cfprintf(w io.Writer, color string, format string, a ...any) {
+	DefaultFormatter.Cfprintf(w, color, format, a...)
+}
+
+func Cprintln(color string, a ...any) {
+	DefaultFormatter.Cprintln(color, a...)
+}
+
+func Cfprintln(w io.Writer, color string, a ...any) {
+	DefaultFormatter.Cfprintln(w, color, a...)
+}
+
+func Begin(decor ansi.Decor) {
+	DefaultFormatter.Begin(decor)
+}
+
+func End() {
+	DefaultFormatter.End()
 }
