b399b410b9d0329b63c0e50a9de6d519583d376d
Tobias Hort <mail@tobiashort.com>
2026-03-31 07:40:57 +0200

    get current position of cursor

--- lib/ansi/cursor.go
+++ lib/ansi/cursor.go
@@ -1,6 +1,13 @@
 package ansi
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/tobiashort/th-utils/lib/must"
+)
 
 type CursorControl = string
 
@@ -8,6 +15,9 @@ const (
 	CursorHide               CursorControl = "\033[?25l"
 	CursorShow               CursorControl = "\033[?25h"
 	CursorMoveToHomePosition CursorControl = "\033[H"
+	CursorSavePosition       CursorControl = "\033[s"
+	CursorRestorePosition    CursorControl = "\033[u"
+	CursorCurrentPosition    CursorControl = "\033[6n"
 )
 
 func CursorMoveTo(line, column int) CursorControl {
@@ -32,4 +42,14 @@ func CursorMoveLeft(columns int) CursorControl {
 
 func CursorMoveToColumn(column int) CursorControl {
 	return fmt.Sprintf("\033[%dG", column)
+}
+
+func CursorGetCurrentPosition() (row int, col int) {
+	fmt.Fprint(os.Stderr, CursorCurrentPosition)
+	reader := bufio.NewReader(os.Stdin)
+	response := must.Do2(reader.ReadString('R'))
+	response = strings.TrimPrefix(response, "\x1b[")
+	response = strings.TrimSuffix(response, "R")
+	fmt.Sscanf(response, "%d;%d", &row, &col)
+	return
 }
