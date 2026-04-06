18b121d604b9af4a488da39c77e04f5d0af7c2b5
Tobias Hort <mail@tobiashort.com>
2026-04-06 07:15:15 +0200

    forward arguments

--- build_nocgo.go
+++ build_nocgo.go
@@ -9,6 +9,9 @@ import (
 
 func main() {
 	cmd := exec.Command("go", "run", ".")
+	if len(os.Args) > 1 {
+		cmd.Args = append(cmd.Args, os.Args[1:]...)
+	}
 	cmd.Env = os.Environ()
 	cmd.Env = append(cmd.Env, "CGO_ENABLED=1")
 	cmd.Env = append(cmd.Env, "CC=zig cc")
